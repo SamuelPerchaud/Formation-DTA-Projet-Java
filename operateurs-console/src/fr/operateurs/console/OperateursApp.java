0332de48888f767920eb086cbf0f36c1b59c1d23
samuel perchaud <perchaud.samuel@gmail.com>
2016-04-26 14:34:50 +0200

    mise a jour

---
+++ operateurs-console/src/fr/operateurs/console/OperateursApp.java
@@ -0,0 +1,25 @@
+package fr.operateurs.console;
+
+import java.util.Scanner;
+
+public class OperateursApp {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("veuillez saisir un premier nombre");
+		double nb = sc.nextDouble();
+		System.out.println("votre premier nombre est : " + nb);
+		System.out.println("veuillez saisir un deuxieme nombre");
+		double nb2 = sc.nextDouble();
+		System.out.println("votre deuxieme nombre est : " + nb2);
+		System.out.println("addition     :"+nb+" + "+nb2+" = " + (nb+nb2));
+		System.out.println("différence   :"+nb+" - "+nb2+" = " + (nb-nb2));
+		System.out.println("produit      :"+nb+" * "+nb2+" = " + (nb*nb2));
+		System.out.println("division     :"+nb+" / "+nb2+" = " + (nb/nb2));
+		System.out.println("modulo       :"+nb+" % "+nb2+" = " + (nb%nb2));
+
+		sc.close();
+	}
+
+}
