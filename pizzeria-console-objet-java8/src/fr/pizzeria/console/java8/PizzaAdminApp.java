091b215d51c0cbe965a6509c453e29de5980062a
samuel perchaud <perchaud.samuel@gmail.com>
2016-05-06 13:39:51 +0200

    test

---
+++ pizzeria-console-objet-java8/src/fr/pizzeria/console/java8/PizzaAdminApp.java
@@ -0,0 +1,21 @@
+package fr.pizzeria.console.java8;
+
+import java.util.Scanner;
+
+import fr.pizzeria.dao.IPizzaDao;
+import fr.pizzeria.dao.PizzaDaoImpl;
+import fr.pizzeria.ihm.menu.Menu;
+
+public class PizzaAdminApp {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) {
+			IPizzaDao dao = new PizzaDaoImpl();
+			Menu menu = new Menu(sc,dao);
+			menu.afficher();
+		}
+		
+	}
+
+}
+//test
