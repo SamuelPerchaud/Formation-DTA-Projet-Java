a196a4f211a28774587ccb74a4af3c34e58f981c
samuel perchaud <perchaud.samuel@gmail.com>
2016-05-09 16:26:39 +0200

    utilisation de test unitaires commit avant passage a MAVEN

---
+++ pizzeria-console-objet-java8/test/fr/pizza/dao/TestPizzaDaoImpl.java
@@ -0,0 +1,92 @@
+package fr.pizza.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.pizzeria.dao.PizzaDaoImpl;
+import fr.pizzeria.exception.DaoException;
+import fr.pizzeria.exception.SavePizzaException;
+import fr.pizzeria.model.CategoriePizza;
+import fr.pizzeria.model.Pizza;
+
+public class TestPizzaDaoImpl {
+
+	//private static final Class<? extends Throwable> SavePizzaException = null;
+	private ArrayList<Pizza> pizzasTest;
+	private PizzaDaoImpl pizzaDao;
+
+	@Before
+	public void setUp() {
+		 pizzaDao = new PizzaDaoImpl();
+		 pizzasTest = new ArrayList<Pizza>();
+		 
+		pizzasTest.add(new Pizza("PEP", "Pépéroni", 12.50, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("MAR", "Margherita", 14.00, CategoriePizza.SANS_VIANDE));
+		pizzasTest.add( new Pizza("REI", "La Reine", 11.50, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("FRO", "La 4 fromages", 12.00, CategoriePizza.SANS_VIANDE));
+		pizzasTest.add( new Pizza("CAN", "La cannibale", 12.50, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("SAV", "La savoyarde", 13.00, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("ORI", "L'orientale", 13.50, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("IND", "L'indienne", 14.00, CategoriePizza.VIANDE));
+		pizzasTest.add( new Pizza("SAU", "La Saumonéta", 14.00, CategoriePizza.POISSON));
+		pizzasTest.sort(Comparator.comparing(Pizza::getCode));
+
+	}
+
+	@Test
+	public void testfindAllPizzas() {
+		// pizzaDao.findAllPizzas().stream()
+		// .forEach(listePizzas);
+
+		List<Pizza> pizzas = pizzaDao.findAllPizzas();
+		pizzas.sort(Comparator.comparing(Pizza::getCode));
+		Pizza[] tabPizza = pizzas.<Pizza>toArray(new Pizza[0]);
+		Pizza[] tabPizzaTest = pizzasTest.<Pizza>toArray(new Pizza[0]);
+		assertArrayEquals(tabPizza, tabPizzaTest);
+		//fail("Not yet implemented");
+
+	}
+
+
+
+
+	/**
+	 * private boolean asserEquals(List<Pizza> pizzas, Map<String, Pizza>
+	 * pizzasTest2) { pizzas.toArray();
+	 * 
+	 * 
+	 * return false; }
+	 */
+	//SavePizzaException
+	@Test(expected = SavePizzaException.class)
+	public void testPizzasavePizza_codeExistant() throws DaoException {
+		Pizza newPizza =  new Pizza("PEP", "Pépéroni", 12.50, CategoriePizza.VIANDE);
+		pizzaDao.savePizza(newPizza);
+		
+		
+		//fail("Not yet implemented");
+
+	}
+
+	@Test
+	public void testupdatePizza() {
+		fail("Not yet implemented");
+
+	}
+
+	@Test
+	public void testdeletePizza() {
+		fail("Not yet implemented");
+
+	}
+
+}
---
+++ pizzeria-console-objet-java8/test/fr/pizzeria/AjouterNouvellePizzaOptionMenuTest.java
@@ -0,0 +1,69 @@
+package fr.pizzeria;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import fr.pizzeria.dao.IPizzaDao;
+import fr.pizzeria.dao.PizzaDaoImpl;
+import fr.pizzeria.exception.DaoException;
+import fr.pizzeria.ihm.menu.option.AjouterNouvellePizzaOptionMenu;
+import fr.pizzeria.model.CategoriePizza;
+import fr.pizzeria.model.Pizza;
+
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
+public class AjouterNouvellePizzaOptionMenuTest {
+
+	@Rule
+	public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();	
+	@Rule
+	public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	private AjouterNouvellePizzaOptionMenu optionMenu;
+	private IPizzaDao pizzaDao;
+	@Before
+	public void setUp() throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		pizzaDao = new PizzaDaoImpl();
+		optionMenu = new AjouterNouvellePizzaOptionMenu(scanner,pizzaDao);
+	}
+
+	@Test
+	public void test() throws DaoException, IOException {
+		systemInMock.provideLines("NEW","Newton","15.25","0");
+		boolean next = optionMenu.execute();
+		assertTrue(next);
+		List<Pizza> allPizzas = pizzaDao.findAllPizzas();
+		Optional<Pizza> pizzaOpt = allPizzas.stream().filter(p -> "NEW".equals(p.getCode())).findFirst();
+		assertTrue(pizzaOpt.isPresent());
+		Pizza pizza = pizzaOpt.get();
+		assertEquals("Newton", pizza.getNom());
+		assertTrue(pizza.getNouveauPrix()== 15.25);
+		assertEquals(CategoriePizza.VIANDE,pizza.getCategorie());
+		
+		String outAttendu = Files.lines(Paths.get("test/fr/pizzeria/ihm/menu/option/resultatAjouterNouvellePizza.txt")).collect(Collectors.joining("\n"));
+		outAttendu +="\n";
+		assertEquals(outAttendu, systemOutRule.getLog());
+		
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
