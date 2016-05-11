9a9da5dfa169d4d88df2cf9bb28695d34636f97b
samuel perchaud <perchaud.samuel@gmail.com>
2016-05-11 12:17:37 +0200

    correction du build defaillant

--- pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzaAdminConsole.java
+++ pizzeria-console-objet-java8/src/main/java/fr/pizzeria/console/PizzaAdminConsole.java
@@ -16,7 +16,6 @@ public class PizzaAdminConsole {
 	static IPizzaDao daoImpl;
 
 	public static void main(String[] args) throws IOException, DaoException {
-		qsdkpgmljhùoqd^fhgôqdùhi
 		//System.err.println("entre prog");
 		ResourceBundle bundle = ResourceBundle.getBundle("application");
 		String confString = bundle.getString("dao.impl");
