b13ddc1b1a8c9f58dcdd57496c20ccdbe96b8d3a
samuel perchaud <perchaud.samuel@gmail.com>
2016-05-13 15:20:05 +0200

    ajout d'un PreparedStatement et une transaction

--- pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoDB.java
+++ pizzeria-dao/src/main/java/fr/pizzeria/dao/PizzaDaoDB.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -67,62 +68,79 @@ public class PizzaDaoDB implements IPizzaDao {
 	@Override
 	public void savePizza(Pizza newPizza) throws DaoException, SQLException {
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzeria", "root", "");
-		Statement statement = connection.createStatement();
+		connection.setAutoCommit(false);
+		
+		
+		
+		PreparedStatement statement = connection.prepareStatement("INSERT INTO `pizza` (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES (NULL, ?, ?, ?, ?)");
+		statement.setString(1,newPizza.getCode());
+		statement.setString(2, newPizza.getNom());
+		statement.setDouble(3, newPizza.getNouveauPrix());
+		statement.setString(4, newPizza.getCategorie().toString());
 		// ResultSet resultats = statement.executeQuery("INSERT INTO `pizza`
 		// (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES
 		// (NULL,'"+newPizza.getId()+"','"+newPizza.getNom()+"','"+newPizza.getNouveauPrix()+"','"+newPizza.getCategorie().toString()+"'");
-
+		int nbpizzas = statement.executeUpdate();
+		/**
 		int nbpizzas = statement.executeUpdate(String.format(
 				"INSERT INTO `pizza` (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES (NULL, '%s', '%s', '%s', '%s')",
 				newPizza.getCode(), newPizza.getNom(), newPizza.getNouveauPrix(), newPizza.getCategorie().toString()));
+				*/
+		if (nbpizzas == 1) {
+			connection.commit();
+			System.out.println(nbpizzas + " pizza inséré");
 
-		System.out.println(nbpizzas + " pizza inséré");
+		} else {
+			connection.rollback();
+			System.err.println("pas de  pizza inséré");
+
+		}
 
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException, SQLException {
 		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzeria", "root", "");
-				Statement statement = connection.createStatement();){
-				// ResultSet resultats = statement.executeQuery("INSERT INTO `pizza`
-				// (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES
-				// (NULL,'"+newPizza.getId()+"','"+newPizza.getNom()+"','"+newPizza.getNouveauPrix()+"','"+newPizza.getCategorie().toString()+"'");
+				Statement statement = connection.createStatement();) {
+			// ResultSet resultats = statement.executeQuery("INSERT INTO `pizza`
+			// (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES
+			// (NULL,'"+newPizza.getId()+"','"+newPizza.getNom()+"','"+newPizza.getNouveauPrix()+"','"+newPizza.getCategorie().toString()+"'");
 
-				int nbpizzas = statement.executeUpdate(String.format(
-						"UPDATE `pizza`SET CODE='%s',NOM='%s',PRIX='%s',CATEGORIE='%s' WHERE CODE = '%s'",updatePizza.getCode(),updatePizza.getNom(),updatePizza.getNouveauPrix(),updatePizza.getCategorie().toString(),codePizza));
-						//newPizza.getCode(), newPizza.getNom(), newPizza.getNouveauPrix(), newPizza.getCategorie().toString()));
+			int nbpizzas = statement.executeUpdate(
+					String.format("UPDATE `pizza`SET CODE='%s',NOM='%s',PRIX='%s',CATEGORIE='%s' WHERE CODE = '%s'",
+							updatePizza.getCode(), updatePizza.getNom(), updatePizza.getNouveauPrix(),
+							updatePizza.getCategorie().toString(), codePizza));
+			// newPizza.getCode(), newPizza.getNom(), newPizza.getNouveauPrix(),
+			// newPizza.getCategorie().toString()));
 
-				System.out.println(nbpizzas + " pizza mise a jour");
+			System.out.println(nbpizzas + " pizza mise a jour");
 
-				
-				}catch (SQLException e){
-					throw new SQLException(e);
-				}
-				//DELETE FROM `pizza` WHERE `pizza`.`ID` = 4
-				
+		} catch (SQLException e) {
+			throw new SQLException(e);
+		}
+		// DELETE FROM `pizza` WHERE `pizza`.`ID` = 4
 
 	}
 
 	@Override
 	public void deletePizza(String codePizza) throws DaoException, SQLException {
 		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzeria", "root", "");
-		Statement statement = connection.createStatement();){
-		// ResultSet resultats = statement.executeQuery("INSERT INTO `pizza`
-		// (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES
-		// (NULL,'"+newPizza.getId()+"','"+newPizza.getNom()+"','"+newPizza.getNouveauPrix()+"','"+newPizza.getCategorie().toString()+"'");
+				Statement statement = connection.createStatement();) {
+			// ResultSet resultats = statement.executeQuery("INSERT INTO `pizza`
+			// (`ID`, `CODE`, `NOM`, `PRIX`, `CATEGORIE`) VALUES
+			// (NULL,'"+newPizza.getId()+"','"+newPizza.getNom()+"','"+newPizza.getNouveauPrix()+"','"+newPizza.getCategorie().toString()+"'");
 
-		int nbpizzas = statement.executeUpdate(String.format(
-				"DELETE FROM `pizza` WHERE CODE = '%s'",codePizza));
-				//newPizza.getCode(), newPizza.getNom(), newPizza.getNouveauPrix(), newPizza.getCategorie().toString()));
+			int nbpizzas = statement.executeUpdate(String.format("DELETE FROM `pizza` WHERE CODE = '%s'", codePizza));
+			// newPizza.getCode(), newPizza.getNom(), newPizza.getNouveauPrix(),
+			// newPizza.getCategorie().toString()));
 
-		System.out.println(nbpizzas + " pizza supprimé");
+			System.out.println(nbpizzas + " pizza supprimé");
 
-		
-		}catch (SQLException e){
+		} catch (SQLException e) {
 			throw new SQLException(e);
 		}
-		//DELETE FROM `pizza` WHERE `pizza`.`ID` = 4
-		
+		// DELETE FROM `pizza` WHERE `pizza`.`ID` = 4
+
 	}
 
 }
