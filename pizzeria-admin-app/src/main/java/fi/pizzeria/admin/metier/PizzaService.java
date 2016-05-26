a8803e36a64086825ca31ab1c6bbf9800dbbeb35
samuel perchaud <perchaud.samuel@gmail.com>
2016-05-26 17:32:32 +0200

    commit du jeudi soir
    utilisation de JPA dans un EJB

--- pizzeria-admin-app/src/main/java/fi/pizzeria/admin/metier/PizzaService.java
+++ pizzeria-admin-app/src/main/java/fi/pizzeria/admin/metier/PizzaService.java
@@ -1,41 +1,127 @@
 package fi.pizzeria.admin.metier;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import org.apache.commons.collections4.ListUtils;
 
 import fr.pizzeria.dao.pizza.IPizzaDao;
 import fr.pizzeria.exception.DaoException;
+import fr.pizzeria.model.CategoriePizza;
 import fr.pizzeria.model.Pizza;
 
+@Stateless
 public class PizzaService  {
 	@Inject private IPizzaDao pizzaDao;
-
+	@PersistenceContext(unitName="pizzeria-admin-app") private EntityManager em;
 	
 	public List<Pizza> findAllPizzas() throws DaoException, SQLException {
-		// TODO Auto-generated method stub
-		return pizzaDao.findAllPizzas();
+		List<Pizza> listPizza = em.createQuery("SELECT p FROM Pizza p", Pizza.class).getResultList();
+		return listPizza;
 	}
 
 	
 	public void savePizza(Pizza newPizza) throws DaoException, SQLException {
-		 pizzaDao.savePizza(newPizza);		
+		Pizza pizza = null;
+		try {
+			pizza = em.createNamedQuery("pizza.getcode", Pizza.class).setParameter("code", newPizza.getCode())
+					.getSingleResult();
+			System.err.println("INFO---- Pizza deja presente");
+
+		} catch (NoResultException e) {
+			em.persist(newPizza);
+			// System.err.println(newPizza);
+			System.err.println("INFO---- pizza inséré  : " + newPizza);
+			// EntityNotFoundException e
+		}
+
 	}
 
 	
 	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException, SQLException {
-		pizzaDao.updatePizza(codePizza, updatePizza);		
-	}
+		Pizza pizza = em.createNamedQuery("pizza.getcode", Pizza.class).setParameter("code", codePizza)
+				.getSingleResult();
+		if (pizza != null) {
+			pizza.setCode(updatePizza.getCode());
+			pizza.setNom(updatePizza.getNom());
+			pizza.setPrix(updatePizza.getNouveauPrix());
+			pizza.setCategorie(updatePizza.getCategorie());
+		}
+
+		// em.merge(pizza);
+		System.err.println("la pizza : " + pizza + "a été mise a jour");
+		}
 
 	
-	public void deletePizza(String codePizza) throws DaoException, SQLException {
-		pizzaDao.deletePizza(codePizza);
+	public void deletePizza(String codePizza)  {
+		Pizza pizza = em.createNamedQuery("pizza.getcode", Pizza.class).setParameter("code", codePizza)
+				.getSingleResult();
+		em.remove(pizza);
+		System.err.println("la pizza : " + pizza + "a été supprimé");
+
 		
-	}
-
+	}		
 	
-	public void importPizza() throws DaoException, SQLException {
-		pizzaDao.importPizza();		
-	}
+
+	@PostConstruct
+	public void importPizza() {
+		List<List<Pizza>> test = new ArrayList<List<Pizza>>();
+		List<Pizza> listPizza;
+		try {
+			URL resource = Thread.currentThread().getContextClassLoader().getResource("data");
+			this.getClass().getResource("data");
+			 listPizza = Files.list(Paths.get(resource.toURI())).map(path -> {
+				Pizza p = new Pizza();
+				p.setCode(path.getFileName().toString().replaceAll(".txt", ""));
+				try {
+					String ligne = Files.readAllLines(path).get(0);
+					String[] ligneTab = ligne.split(";");
+					p.setNom(ligneTab[0]);
+					p.setPrix(new BigDecimal(ligneTab[1]));
+					p.setCategorie(CategoriePizza.valueOf(ligneTab[2]));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.err.println("INFO----IMPORT de " + p);
+
+				return p;
+			}).collect(Collectors.toList());
+			 test = ListUtils.partition(listPizza, 3);
+
+				for (List<Pizza> listPizzaparcour : test) {
+					for (Pizza pizza : listPizzaparcour) {
+						// System.err.println("INFO----IMPORT de " + pizza);
+						savePizza(pizza);
+					}
+				}	
+		} catch (IOException e) {
+			// TODO logger à faire
+			e.printStackTrace();
+		} catch (DaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
 }
